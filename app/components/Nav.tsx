9aca426ee7eb7e3a29c0244cad12cbce573bb26e
thentrsfs <filipstojkov54@gmail.com>
2026-04-14 17:37:13 +0200

    Change text on nav items

--- app/components/Nav.tsx
+++ app/components/Nav.tsx
@@ -5,7 +5,7 @@ const Nav = () => {
     <nav className="absolute top-0 w-full flex justify-between items-center lg:px-30 p-6 lg:py-8">
         <h1 className="lg:text-5xl text-3xl font-bold font-heading">UrbanEdge</h1>
         <button className="lg:hidden text-lg font-medium">Menu</button>
-        <ul className="lg:flex gap-10 text-xl font-medium items-center hidden">
+        <ul className="lg:flex gap-10 text-lg font-medium tracking-wider items-center hidden uppercase">
             <li><a href="">Shop</a></li>
             <li><a href="">New Drop</a></li>
             <li><a href="">Lookbook</a></li>
