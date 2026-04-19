076fdad841f6603dd657d3099748bba33ee6622a
thentrsfs <filipstojkov54@gmail.com>
2026-04-19 13:17:53 +0200

    Increase z-index on Nav

--- app/components/Nav.tsx
+++ app/components/Nav.tsx
@@ -2,7 +2,7 @@ import { FiShoppingCart } from "react-icons/fi";
 
 const Nav = () => {
   return (
-    <nav className="absolute top-0 w-full flex justify-between items-center lg:px-30 p-6 lg:py-8">
+    <nav className="absolute top-0 w-full flex justify-between items-center lg:px-30 p-6 lg:py-8 z-10">
         <h1 className="lg:text-5xl text-3xl font-bold font-heading">UrbanEdge</h1>
         <button className="lg:hidden text-lg font-medium">Menu</button>
         <ul className="lg:flex gap-10 text-lg font-medium tracking-wider items-center hidden uppercase">
