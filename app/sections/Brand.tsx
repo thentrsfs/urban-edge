f5cfcfdfd56bcc0546a6c847b98967ab43fe03d9
thentrsfs <filipstojkov54@gmail.com>
2026-04-23 10:57:09 +0200

    Add an exit animation too on brand section

--- app/sections/Brand.tsx
+++ app/sections/Brand.tsx
@@ -18,7 +18,7 @@ const Brand = () => {
             scrollTrigger: {
                 trigger: el,
                 start: "top top",
-                end: "+=120%",
+                end: "+=220%",
                 scrub: 1,
                 pin: true,
             }
@@ -28,12 +28,15 @@ const Brand = () => {
     { opacity: 0, filter: 'blur(20px)' ,y: 20 },
     { opacity: 1, filter: 'blur(0px)' , y: 0, duration: 1.2, ease: "power3.out" }
   );
+
+  tl.to('.brand-title', { opacity: 1, filter: 'blur(0px)' ,y: 0, duration: 2, ease: "power3.out" });
+  tl.to('.brand-title', { opacity: 0, filter: 'blur(20px)' ,y: -20, duration: 2, ease: "power3.out" });
       
     }, {scope: sectionRef});
 
   return (
-    <section ref={sectionRef} className="h-dvh flex flex-col items-center justify-center mb-[150vh]">
-        <h1 className="text-8xl brand-title font-bold font-heading tracking-wide">DESIGNED FOR THE UNSEEN</h1>
+    <section ref={sectionRef} className="h-dvh flex flex-col items-center justify-center mb-[180vh]">
+        <h1 className="lg:text-8xl text-6xl brand-title font-bold font-heading tracking-wide px-6 text-center">DESIGNED FOR THE UNSEEN</h1>
     </section>
   )
 }
