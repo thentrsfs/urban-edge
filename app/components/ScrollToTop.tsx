24d36c05ce791bcd3a1467f43cab3dd2f60c28a9
thentrsfs <filipstojkov54@gmail.com>
2026-04-25 21:22:49 +0200

    Set timeout on scroll to top useEffect

--- app/components/ScrollToTop.tsx
+++ app/components/ScrollToTop.tsx
@@ -2,14 +2,16 @@
 
 import { useEffect } from 'react';
 import { usePathname } from 'next/navigation';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
 
 export default function ScrollToTop() {
 	const pathname = usePathname();
-
 	useEffect(() => {
-		requestAnimationFrame(() => {
-			window.scrollTo(0, 0);
-		});
+		window.scrollTo(0, 0);
+
+		setTimeout(() => {
+			ScrollTrigger.refresh();
+		}, 50);
 	}, [pathname]);
 
 	return null;
