d299a842b06bf4b1f2a2633bec064f46dfafc7f1
thentrsfs <filipstojkov54@gmail.com>
2026-04-25 11:20:06 +0200

    Add one more hint for scrolling in Brand section

--- app/sections/Brand.tsx
+++ app/sections/Brand.tsx
@@ -26,6 +26,12 @@ const Brand = () => {
 				},
 			});
 
+			tl.to('.continue-hint', {
+				opacity: 0,
+				duration: 0.6,
+				ease: 'power3.out',
+			});
+
 			tl.fromTo(
 				'.brand-title',
 				{ opacity: 0, filter: 'blur(20px)', y: 20 },
@@ -37,6 +43,15 @@ const Brand = () => {
 					ease: 'power3.out',
 				},
 			);
+			tl.to(
+				'.go-hint',
+				{
+					opacity: 1,
+					duration: 0.6,
+					ease: 'power3.out',
+				},
+				'+=0.6',
+			);
 
 			tl.to('.brand-title', {
 				opacity: 1,
@@ -45,6 +60,12 @@ const Brand = () => {
 				duration: 2,
 				ease: 'power3.out',
 			});
+			tl.to('.go-hint', {
+				opacity: 0,
+				duration: 0.8,
+				ease: 'power3.out',
+			});
+
 			tl.to('.brand-title', {
 				opacity: 0,
 				filter: 'blur(20px)',
@@ -56,41 +77,19 @@ const Brand = () => {
 		{ scope: sectionRef },
 	);
 
-	useGSAP(() => {
-		const el = sectionRef.current;
-		if (!el) return;
-
-		const tl = gsap.timeline({
-			scrollTrigger: {
-				trigger: el,
-				start: 'top 70%',
-				end: '+=100%',
-				scrub: 1,
-			},
-		});
-
-		tl.to('.continue-hint', {
-			opacity: 1,
-			y: 0,
-			ease: 'power3.out',
-		});
-
-		tl.to('.continue-hint', {
-			opacity: 0,
-			ease: 'power3.out',
-		});
-	});
-
 	return (
 		<section
 			ref={sectionRef}
 			className='h-dvh flex flex-col items-center justify-center mb-[180vh] relative'>
-			<div className='absolute top-10 left-1/2 -translate-x-1/2 text-lg font-heading tracking-widest text-muted uppercase items-center flex flex-col continue-hint opacity-0'>
+			<div className='absolute top-10 left-1/2 -translate-x-1/2 text-lg font-heading tracking-widest text-muted uppercase items-center flex flex-col continue-hint'>
 				<span>Continue</span> <MoveDown size={20} />
 			</div>
 			<h1 className='lg:text-8xl text-6xl brand-title font-bold font-heading tracking-wide px-6 text-center'>
 				DESIGNED FOR THE UNSEEN
 			</h1>
+			<div className='absolute bottom-10 left-1/2 -translate-x-1/2 text-lg font-heading tracking-widest text-muted uppercase items-center flex flex-col go-hint opacity-0'>
+				<span>Go Further</span> <MoveDown size={20} />
+			</div>
 		</section>
 	);
 };
