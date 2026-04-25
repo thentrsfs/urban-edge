47f4c4215087dd804512164bac504e605034ee3a
thentrsfs <filipstojkov54@gmail.com>
2026-04-25 21:19:51 +0200

    Add one more fallback in ScrollToTop useEffect

--- app/components/ScrollToTop.tsx
+++ app/components/ScrollToTop.tsx
@@ -7,7 +7,9 @@ export default function ScrollToTop() {
 	const pathname = usePathname();
 
 	useEffect(() => {
-		window.scrollTo(0, 0);
+		requestAnimationFrame(() => {
+			window.scrollTo(0, 0);
+		});
 	}, [pathname]);
 
 	return null;
