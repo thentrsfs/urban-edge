3cf65ca69812337dc4ab5da9308d569258a14e6b
thentrsfs <filipstojkov54@gmail.com>
2026-04-26 21:00:15 +0200

    Show different nav links based on where the user is

--- app/components/ui/Nav.tsx
+++ app/components/ui/Nav.tsx
@@ -1,33 +1,49 @@
 'use client';
 
 import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 
 import { useUI } from '../../context/UIProvider';
 
 const Nav = () => {
+	const pathname = usePathname();
 	const { setIsNavOpen } = useUI();
 
 	return (
 		<nav className='absolute top-0 w-full flex justify-between items-center lg:px-30 p-6 lg:py-8 z-11 text-white'>
-			<h1 className='lg:text-5xl text-3xl font-bold font-heading'>UrbanEdge</h1>
+			<Link href='/'>
+				<h1 className='lg:text-5xl text-3xl font-bold font-heading'>
+					UrbanEdge
+				</h1>
+			</Link>
 			<button
 				onClick={() => setIsNavOpen(true)}
 				className='lg:hidden text-lg font-medium'>
 				Menu
 			</button>
 			<ul className='lg:flex lg:gap-15 tracking-widest text-sm items-center hidden uppercase text-white/80'>
-				<li className='cursor-pointer relative group hover:text-white transition-all duration-300'>
-					<Link href='/shop'>Shop</Link>{' '}
-					<span className='absolute left-0 bottom-0 h-px w-0 bg-white transition-all duration-300 group-hover:w-full' />
-				</li>
-				<li className='cursor-pointer relative group hover:text-white transition-all duration-300'>
-					<Link href='#latest-drops'>Latest Drops</Link>{' '}
-					<span className='absolute left-0 bottom-0 h-px w-0 bg-white transition-all duration-300 group-hover:w-full' />
-				</li>
-				<li className='cursor-pointer relative group hover:text-white transition-all duration-300'>
-					<Link href='#lookbook'>Lookbook</Link>{' '}
-					<span className='absolute left-0 bottom-0 h-px w-0 bg-white transition-all duration-300 group-hover:w-full' />
-				</li>
+				{pathname !== '/' ? (
+					<li className='cursor-pointer relative group hover:text-white transition-all duration-300'>
+						<Link href='/'>Home</Link>{' '}
+						<span className='absolute left-0 bottom-0 h-px w-0 bg-white transition-all duration-300 group-hover:w-full' />
+					</li>
+				) : (
+					<div className='flex lg:gap-15'>
+						<li className='cursor-pointer relative group hover:text-white transition-all duration-300'>
+							<Link href='/shop'>Shop</Link>{' '}
+							<span className='absolute left-0 bottom-0 h-px w-0 bg-white transition-all duration-300 group-hover:w-full' />
+						</li>
+						<li className='cursor-pointer relative group hover:text-white transition-all duration-300'>
+							<Link href='/#latest-drops'>Latest Drops</Link>{' '}
+							<span className='absolute left-0 bottom-0 h-px w-0 bg-white transition-all duration-300 group-hover:w-full' />
+						</li>
+						<li className='cursor-pointer relative group hover:text-white transition-all duration-300'>
+							<Link href='#lookbook'>Lookbook</Link>{' '}
+							<span className='absolute left-0 bottom-0 h-px w-0 bg-white transition-all duration-300 group-hover:w-full' />
+						</li>
+					</div>
+				)}
+
 				<li className='cursor-pointer relative group hover:text-white transition-all duration-300'>
 					<Link href='/cart'>Cart</Link>{' '}
 					<span className='absolute left-0 bottom-0 h-px w-0 bg-white transition-all duration-300 group-hover:w-full group-hover:left-0' />
--- app/components/ui/NavMobile.tsx
+++ app/components/ui/NavMobile.tsx
@@ -2,28 +2,59 @@
 
 import { X } from 'lucide-react';
 import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 
 import { useUI } from '../../context/UIProvider';
 const NavMobile = () => {
+	const pathname = usePathname();
 	const { isNavOpen, setIsNavOpen } = useUI();
 	return (
 		<nav
 			className={`fixed inset-0 bg-bg text-white flex flex-col justify-center items-center transition-all duration-300 ${isNavOpen ? 'opacity-100 z-99' : 'opacity-0'}`}>
 			<ul className='flex flex-col gap-15 tracking-widest items-center uppercase text-white/80'>
+				{pathname === '/' ? (
+					<>
+						<li className='cursor-pointer relative group hover:text-white transition-all duration-300'>
+							<Link
+								href='/shop'
+								onClick={() => setIsNavOpen(false)}>
+								Shop
+							</Link>{' '}
+							<span className='absolute left-0 bottom-0 h-px w-0 bg-white transition-all duration-300 group-hover:w-full' />
+						</li>
+						<li className='cursor-pointer relative group hover:text-white transition-all duration-300'>
+							<Link
+								href='#latest-drops'
+								onClick={() => setIsNavOpen(false)}>
+								Latest Drops
+							</Link>{' '}
+							<span className='absolute left-0 bottom-0 h-px w-0 bg-white transition-all duration-300 group-hover:w-full' />
+						</li>
+						<li className='cursor-pointer relative group hover:text-white transition-all duration-300'>
+							<Link
+								href='#lookbook'
+								onClick={() => setIsNavOpen(false)}>
+								Lookbook
+							</Link>{' '}
+							<span className='absolute left-0 bottom-0 h-px w-0 bg-white transition-all duration-300 group-hover:w-full' />
+						</li>
+					</>
+				) : (
+					<li className='cursor-pointer relative group hover:text-white transition-all duration-300'>
+						<Link
+							href='/'
+							onClick={() => setIsNavOpen(false)}>
+							Home
+						</Link>{' '}
+						<span className='absolute left-0 bottom-0 h-px w-0 bg-white transition-all duration-300 group-hover:w-full' />
+					</li>
+				)}
 				<li className='cursor-pointer relative group hover:text-white transition-all duration-300'>
-					<Link href='/shop'>Shop</Link>{' '}
-					<span className='absolute left-0 bottom-0 h-px w-0 bg-white transition-all duration-300 group-hover:w-full' />
-				</li>
-				<li className='cursor-pointer relative group hover:text-white transition-all duration-300'>
-					<Link href='#latest-drops'>Latest Drops</Link>{' '}
-					<span className='absolute left-0 bottom-0 h-px w-0 bg-white transition-all duration-300 group-hover:w-full' />
-				</li>
-				<li className='cursor-pointer relative group hover:text-white transition-all duration-300'>
-					<Link href='#lookbook'>Lookbook</Link>{' '}
-					<span className='absolute left-0 bottom-0 h-px w-0 bg-white transition-all duration-300 group-hover:w-full' />
-				</li>
-				<li className='cursor-pointer relative group hover:text-white transition-all duration-300'>
-					<Link href='/cart'>Cart</Link>{' '}
+					<Link
+						href='/cart'
+						onClick={() => setIsNavOpen(false)}>
+						Cart
+					</Link>{' '}
 					<span className='absolute left-0 bottom-0 h-px w-0 bg-white transition-all duration-300 group-hover:w-full group-hover:left-0' />
 				</li>
 			</ul>
