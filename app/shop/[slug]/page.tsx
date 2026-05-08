24bd66a50f57b66a1895636d783b249ef648107a
thentrsfs <filipstojkov54@gmail.com>
2026-05-08 21:50:28 +0200

    Add 'You may also like' section below the product in product page

--- app/shop/[slug]/page.tsx
+++ app/shop/[slug]/page.tsx
@@ -1,6 +1,9 @@
 import { products, latestProducts } from '@/app/data/products';
 import { notFound } from 'next/navigation';
 import Image from 'next/image';
+import { MoveRight } from 'lucide-react';
+
+import Link from 'next/link';
 
 const ProductPage = async ({
 	params,
@@ -10,14 +13,17 @@ const ProductPage = async ({
 	const { slug } = await params;
 	const allProducts = [...products, ...latestProducts];
 	const product = allProducts.find((p) => p.slug === slug);
+	const relatedProducts = products
+		.filter((p) => product && p.id !== product.id)
+		.slice(0, 3);
 
 	if (!product) {
 		notFound();
 	}
 
 	return (
-		<div className='min-h-screen flex flex-col items-center justify-center font-bold text-white lg:py-30 py-6'>
-			<div className='grid grid-cols-1 lg:grid-cols-2 gap-10 items-center lg:w-7xl'>
+		<div className='min-h-screen flex flex-col items-center justify-center font-bold text-white lg:p-30 p-6'>
+			<section className='grid grid-cols-1 lg:grid-cols-2 gap-10 items-center lg:w-7xl'>
 				<div className='lg:w-full lg:h-[75vh] relative overflow-hidden'>
 					<Image
 						src={product.image}
@@ -40,10 +46,39 @@ const ProductPage = async ({
 						Add to cart
 					</button>
 				</div>
-			</div>
-			<div className='mt-20 text-center'>
-				<h2>You may also like</h2>
-			</div>
+			</section>
+			<section className='mt-40 border-t border-muted/50 pt-16 w-full'>
+				<h2 className='text-3xl uppercase font-heading tracking-wide mb-10 text-center'>
+					You may also like
+				</h2>
+				<div className='grid grid-cols-1 md:grid-cols-3 gap-10 lg:w-7xl mx-auto'>
+					{relatedProducts.map((p) => (
+						<Link
+							href={`/shop/${p.slug}`}
+							key={p.id}>
+							<div className='relative group h-100 overflow-hidden rounded after:absolute after:inset-0 after:bg-black/10'>
+								<Image
+									src={p.image}
+									alt={p.name}
+									fill
+									sizes='(max-width: 768px) 90vw, 33vw'
+									className='object-cover group-hover:scale-105 transition-all duration-500'
+								/>
+								<div className='absolute inset-0 bg-transparent group-hover:bg-black/30 group-hover:backdrop-blur-[2px] transition-all duration-500'>
+									<p className='text-center absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 opacity-0 group-hover:opacity-100 transition-all duration-500 text-white uppercase tracking-widest font-semibold'>
+										View Product <MoveRight className='inline-block' />
+									</p>
+								</div>
+							</div>
+							<div className='mt-4 text-center'>
+								<h3 className='uppercase font-heading text-2xl'>{p.name}</h3>
+
+								<p className='text-white/60 mt-1'>{p.price} Kč</p>
+							</div>
+						</Link>
+					))}
+				</div>
+			</section>
 		</div>
 	);
 };
