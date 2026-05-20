4e7312f770a61511b1bd6dacc378ffaf6f185717
thentrsfs <filipstojkov54@gmail.com>
2026-05-20 19:00:13 +0200

    Force scrolling to the top when the entire page finishes loading.

--- app/components/system/ScrollToTop.tsx
+++ app/components/system/ScrollToTop.tsx
@@ -4,9 +4,21 @@ import { useEffect } from 'react';
 
 export default function ScrollToTop() {
 	useEffect(() => {
+		const scrollToTop = () => {
+			window.scrollTo({ top: 0, left: 0, behavior: 'instant' });
+		};
+
 		window.history.scrollRestoration = 'manual';
 
-		window.scrollTo(0, 0);
+		requestAnimationFrame(() => {
+			requestAnimationFrame(scrollToTop);
+		});
+
+		window.addEventListener('load', scrollToTop);
+
+		return () => {
+			window.removeEventListener('load', scrollToTop);
+		};
 	}, []);
 
 	return null;
